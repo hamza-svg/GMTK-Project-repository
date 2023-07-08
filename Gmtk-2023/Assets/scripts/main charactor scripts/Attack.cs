e5b09f8a19f9f3fd92f0bd8787471de1794408e3
hamza <hzansari786@gmail.com>
2023-07-08 11:05:48 +0530

    attack script

---
+++ Gmtk-2023/Assets/scripts/main charactor scripts/Attack.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public float radius = 1f;
+    public LayerMask layerMask;
+    public Transform target;
+
+    private void Awake()
+    {
+        gameObject.GetComponent<transformation>();
+    }
+    private void Update()
+    {
+        // Perform the overlap circle check
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(target.position, radius, layerMask);
+
+        // Process the colliders that are within the overlap circle
+        foreach (Collider2D collider in colliders)
+        {
+            // Do something with the collider (e.g., apply an effect, trigger an event)
+            Debug.Log("Collided with: " + collider.gameObject.name);
+
+            // Example: Check if the collided object has a specific tag
+            if (collider.CompareTag("enemy"))
+            {                
+
+                if (gameObject.GetComponent<transformation>().isWarewolf == true)
+                {
+                    Debug.Log("Hit an obstacle!");
+                    if (Input.GetKey(KeyCode.Mouse0))
+                    {
+                        Destroy(collider.gameObject);
+                    }
+                    collider.gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+                }
+            }
+            else if(collider.CompareTag("enemy") == null)
+            {
+                collider.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Visualize the overlap circle in the Scene view
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(target.position, radius);
+    }
+}
