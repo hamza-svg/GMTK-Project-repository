6176a1a58e5967b39cf2cf2707e59beabbfd752e
hamza <hzansari786@gmail.com>
2023-07-09 10:58:53 +0530

    player attack but a bit brocken

--- GMTK do all again/Assets/scripts/playerScripts/PlayerMovement.cs
+++ GMTK do all again/Assets/scripts/playerScripts/PlayerMovement.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
+    public float Rotatespeed = 5f;
+   
     public Rigidbody2D rb;
     public Camera cam;
 
     Vector2 movement;
-    Vector2 mousePos;
 
     // Update is called once per frame
     void Update()
@@ -18,15 +18,18 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-    }
 
-    private void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        Vector2 moveANDlook = new Vector2(movement.x, movement.y);
+    
 
-        Vector2 lookDir = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = angle;
+     if (movement.magnitude > 0)
+     {
+            float targetAngle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg - 90;
+            Quaternion targetRotation = Quaternion.Euler(0f, 0f, targetAngle);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Rotatespeed * Time.deltaTime);
+     }
+
+         // Move the character using the movement vector
+         transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
     }
 }
---
+++ GMTK do all again/Assets/scripts/playerScripts/attack.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class attack : MonoBehaviour
+{
+    public float radius = 5f;
+    public LayerMask enemyLayer;
+    public float jumpForce = 10f;
+
+    public GameObject target;
+
+    private void Update()
+    {
+        if (gameObject.GetComponent<Transformation>().isWareWolf)
+        { // Check if the space key is pressed
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // Perform the overlap circle check
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(target.transform.position, radius, enemyLayer);
+
+                if (colliders.Length > 0)
+                {
+                    // Find the closest enemy
+                    Collider2D closestEnemy = colliders[0];
+                    float closestDistance = Vector2.Distance(transform.position, closestEnemy.transform.position);
+
+                    foreach (Collider2D collider in colliders)
+                    {
+                        float distance = Vector2.Distance(transform.position, collider.transform.position);
+                        if (distance < closestDistance)
+                        {
+                            closestEnemy = collider;
+                            closestDistance = distance;
+                        }
+                    }
+
+                    // Jump towards the closest enemy
+                    Vector2 jumpDirection = closestEnemy.transform.position - transform.position;
+                    GetComponent<Rigidbody2D>().AddForce(jumpDirection.normalized * jumpForce, ForceMode2D.Impulse);
+                    
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Visualize the overlap circle in the Scene view
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(target.transform.position, radius);
+    }
+}
